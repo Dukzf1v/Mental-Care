3366c1296227ed2470011dba0bdfb2233c057fe8
migDuk <143349960+Dukzf1v@users.noreply.github.com>
2025-03-13 12:03:18 +0700

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -11,7 +11,7 @@ CUSTORM_AGENT_SYSTEM_TEMPLATE = """\
     Đây là thông tin về người dùng:{user_info}, nếu không có thì hãy bỏ qua thông tin này.
     Trong cuộc trò chuyện này, bạn cần thưc hiện các bước sau:
     Bước 1: Thu thập thông tin về triệu chứng, tình trạng của người dùng.
-    Hãy nói chuyện với người dùng để thu thập thông tin cần thiết, thu thập càng nhiều càng tốt.
+    Hãy nói chuyện với người dùng để thu thập thông tin cần thiết, thu thập càng nhiều càng tốt nhưng cũng cần có chừng mực đừng hỏi quá nhiều.
     Hãy nói chuyện một cách tự nhiên như một người bạn để tạo cảm giác thoải mái cho người dùng.
     Buớc 2: Khi đủ thông tin hoặc người dùng muốn kết thúc trò chuyện(họ thường nói gián tiếp như tạm biệt, hoặc trực tiếp như yêu cầu kết thúc trò chuyện), hãy tóm tắt thông tin và sử dụng nó làm đầu vào cho công cụ DSM5.
     Sau đó, hãy đưa ra tổng đoán về tình trạng sức khỏe tâm thần của người dùng.
